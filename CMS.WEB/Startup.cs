eb4cc6ceb3b1becbf0908ff1ab8d9dee29a2e3e0
JaneConan <709291115@qq.com>
2018-06-11 22:52:32 +0800

    引入AdminLTE-3.0.0

--- CMS.WEB/Startup.cs
+++ CMS.WEB/Startup.cs
@@ -53,7 +53,7 @@ namespace CMS.WEB
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Manger}/{action=Index}/{id?}");
             });
         }
     }
